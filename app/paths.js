88d9197e4df97ccc5ed95bade49470f0df3a46a9
andydevs <akanshul97@gmail.com>
2023-01-28 01:36:40 -0500

    Create tree graph in paths.js

--- app/paths.js
+++ app/paths.js
@@ -5,42 +5,70 @@
  * Created: 10 - 26 - 2020
  */
 import './style/paths.scss'
+import go from 'gojs'
 import story from './story'
 
-function showOptions(id, root) {
-    // Get part
-    let part = story.findPart(id)
-    root.classList.add('part-list')
+const pathsDiagram = new go.Diagram("paths-diagram", {
+    isReadOnly: true
+})
 
-    // Show leaf if part is leaf
-    if (part.id === '~') {
-        let node = document.createElement('li')
-        node.classList.add('unwritten')
-        node.innerText = 'Unwritten...'
-        root.appendChild(node)
-        return
-    }
+pathsDiagram.nodeTemplate = 
+    new go.Node("Auto")
+        .add(new go.Shape({ 
+                figure: 'RoundedRectangle',
+                stroke: null
+            })
+            .bind("fill", "color"))
+        .add(new go.TextBlock("Sample Text", { 
+                margin: 6, 
+                stroke: "black", 
+                font: "12px Verdana" 
+            })
+            .bind('text', 'text'));
 
-    // Show ending if part is an ending
+pathsDiagram.linkTemplate = 
+    new go.Link({
+        routing: go.Link.Orthogonal,
+        corner: 5
+    })
+    .add(new go.Shape({ strokeWidth: 1, stroke: '#777' }))
+
+pathsDiagram.layout = new go.TreeLayout({
+    angle: 0,
+    nodeSpacing: 10,
+    layerSpacing: 30
+})
+
+let colors = [
+    'aqua',
+    'lime',
+    '#ffdd00',
+    'lightsalmon',
+    'violet'
+]
+let nodes = []
+for (const part of story.parts) {
     if (part.theend) {
-        let node = document.createElement('li')
-        node.classList.add('ending')
-        node.innerText = part.ending.toUpperCase()
-        root.appendChild(node)
-        return
-    }
-
-    // Create option list
-    for (let option of part.options) {
-        let node = document.createElement('li')
-        node.classList.add('option')
-        node.innerText = option.text
-        let sublist = document.createElement('ul')
-        showOptions(option.id, sublist)
-        node.appendChild(sublist)
-        root.appendChild(node)
+        nodes.push({
+            key: `${part.id}-end`,
+            parent: part.id,
+            text: part.ending,
+            color: 'transparent'
+        })
+    } else {
+        for (const index in part.options) {
+            let option = part.options[index]
+            nodes.push({
+                key: option.id,
+                parent: part.id,
+                text: option.text,
+                color: colors[index]
+            })
+        }
     }
 }
 
-let root = document.getElementById('paths')
-showOptions('$', root)
+pathsDiagram.model = new go.TreeModel([
+    { key: "$", parent: '', text: "Start", color: "#aaa"},
+    ...nodes
+])
